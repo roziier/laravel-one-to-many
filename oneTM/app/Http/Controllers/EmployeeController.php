a1429c449f28898d76a7e4103d6e8c68cdbb92b4
roziier <rikirosa93@gmail.com>
2021-02-10 15:53:40 +0100

    validate task

--- oneTM/app/Http/Controllers/EmployeeController.php
+++ oneTM/app/Http/Controllers/EmployeeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Employee;
+
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
--- oneTM/app/Http/Controllers/TaskController.php
+++ oneTM/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Task;
 use App\Employee;
 use App\Typology;
+use Illuminate\Support\Facades\Validator;
 
 
 use Illuminate\Http\Request;
@@ -30,14 +31,24 @@ class TaskController extends Controller
 
     public function taskStore(Request $request) {
         $data = $request -> all();
-        
+
+        Validator::make($request -> all(), [
+			'title' => 'required|max:20',
+			'desc' => 'required|min:5|max:200',
+		]) -> validate();
+
         $emp = Employee::findOrFail($data['employee_id']);
         
         $task = Task::make($request -> all());
         $task -> employee() -> associate($emp);
         $task -> save();
 
-        $typs = Typology::findOrFail($data['typs']);
+        if (array_key_exists('tasks', $data)) {
+			$typs = Typology::findOrFail($data['typs']);
+		} else {
+			$typs = [];
+        }
+        
         $task -> typologies() -> attach($typs);
         
         return redirect() -> route('task-index');
@@ -55,13 +66,24 @@ class TaskController extends Controller
     public function taskUpdate(Request $request, $id) {
         $data = $request -> all();
 
+        Validator::make($request -> all(), [
+			'title' => 'required|max:20',
+			'desc' => 'required|min:5|max:200',
+		]) -> validate();
+
         $emp = Employee::findOrFail($data['employee_id']);
         $task = Task::findOrFail($id);
         $task -> update($data);
         $task -> employee() -> associate($emp);
         $task -> save();
 
-        $typs = Typology::findOrFail($data['typs']);
+        if (array_key_exists('tasks', $data)) {
+			$typs = Typology::findOrFail($data['typs']);
+		} else {
+			$typs = [];
+        }
+
+        
         $task -> typologies() -> sync($typs);
 
         return redirect() -> route('task-index');
